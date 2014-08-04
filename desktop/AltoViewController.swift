51c66577531191b5f5a0f42e2fe29d17a93240af
Chad Eubanks <ceubanks@thecodeboutique.com>
2014-08-04 10:36:32 -0700

    Adds state_machine framework: AltoState and Alto Statechart class.Adds desktop framework: AltoViewController class.

---
+++ desktop/AltoViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AltoViewController.swift
+//  BreakingBugs
+//
+//  Created by chad eubanks on 7/28/14.
+//  Copyright (c) 2014 The Code Boutique. All rights reserved.
+//
+
+import Cocoa
+
+class AltoViewController: NSViewController {
+    
+    init(viewName view: NSView) {
+        super.init(nibName: nil, bundle: nil)
+        self.view = view
+    }
+    
+}
